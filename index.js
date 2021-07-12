e8dc838827afdebe4f3e6941a7171237611f67f1
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-07-13 01:25:24 +0300

    first commit for the project

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('test')
+//verileri listeleme
+let ul=document.querySelector('#dataList')
+let liste=document.querySelector('#liste');
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then((response)=>response.json())
+// console.log(data)
+.then((data)=>{
+    data.map(item=>{
+    const foodList=document.createElement('li');
+    foodList.innerHTML=item.title;
+    ul.appendChild(foodList);
+    console.log(foodList)
+})
+let searchBar=document.getElementById('searchBar');
+searchBar && searchBar.addEventListener('keyup',function(){
+    console.log("tıkla bakalım ")
+})    
+    
+    
+})
+.catch((err)=>console.log(err));
+//Kullanıcı bilgisinin gösterimi
+fetch("https://jsonplaceholder.typicode.com/users/1")
+.then(res => res.json())
+.then(nameData => {
+  document.getElementById("myName").innerHTML = "Merhaba, " + nameData.name + "<br>" +" Hoşgeldiniz";
+});
